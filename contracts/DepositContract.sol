611df523da9190b5e245e55c2d49d75efec1afc2
Szegoo <73715684+Szegoo@users.noreply.github.com>
2021-04-13 14:07:10 +0200

    Add files via upload

--- contracts/DepositContract.sol
+++ contracts/DepositContract.sol
@@ -3,22 +3,22 @@ import "./MojContract.sol";
 
 contract DepositContract is MojContract {
     uint256 public totalSupply;
-    mapping(address => uint256) balances;
+    mapping(address => uint256) public balances;
 
     function buyToken() public payable {
         uint256 amount = (msg.value / (10**18)) * 10;
         balances[msg.sender] = amount;
     }
 
-    function TransferToETH(uint256 _amount) public {
-        require(balances[msg.sender] >= _amount);
+    function transferToETH(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "not enough tokens");
         balances[msg.sender] -= _amount;
         uint256 inEth = (_amount * (10**18)) / 10;
         msg.sender.transfer(inEth);
     }
 
-    function Broj(uint256 broj) internal pure returns (uint256) {
-        return sum(broj, broj);
+    function broj(uint256 _broj) internal pure returns (uint256) {
+        return sum(_broj, _broj);
     }
 
     function balance() public view returns (uint256) {
